371b802533ad2d3bdc181138d1b44fe3412e52a2
AneCode <75664942+AneCode@users.noreply.github.com>
2022-01-09 10:40:04 +0530

    Update MissingNUM.cpp

--- IntroductoryP/MissingNUM.cpp
+++ IntroductoryP/MissingNUM.cpp
@@ -1,21 +1,19 @@
-// A C/C++ Program to generate OTP (One Time Password)
-
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+int  main()
 {
-	int n;
+     long long  n;
 	cin>>n;
-	int sum=0;
-	int arr[n-1];
-	for(int i=0;i<n-1;i++){
+	long long sum=0;
+	long long arr[n-1];
+	for(long long i=0;i<n-1;i++){
 	    cin>>arr[i];
 	}
-	for(int i=0;i<n-1;i++){
+	for(long long i=0;i<n-1;i++){
 	    sum+=arr[i];
 	}
-	int total=n*(n+1)/2;
+	long long total=n*(n+1)/2;
 	cout<<total-sum;
 	
 	return(0);
